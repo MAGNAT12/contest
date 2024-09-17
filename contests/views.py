ef6d06150416171b27c29d74ba453195e0ed9277
Magnat12 <rizamatmu@gmail.com>
2024-09-17 21:07:31 +0500

    update 0.1

--- contests/views.py
+++ contests/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
-from .models import Conform
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ConformCreateForm
+from .models import  Conform
 
 def con(request):
     if request.method == 'POST':
         form = ConformCreateForm(request.POST)
         if form.is_valid():
-            input_text = form.cleaned_data['name']  # Используем данные из формы
+            input_text = form.cleaned_data['name']
             con = Conform.objects.create(name=input_text)
             con.save()
+            messages.success(request, 'Данные успешно отправлены!')
+            return redirect('con')  # Перенаправление для предотвращения повторной отправки формы
+        else:
+            messages.error(request, 'Произошла ошибка при отправке данных.')
     else:
         form = ConformCreateForm()
 
